db92e1c4d611bcb38c85c90b42db8dcbbcce07d1
Юсу Салихаров <yususaliharov@MacBook-Air.local>
2023-04-18 20:04:46 +0600

    order 2

---
+++ apps/order/urls.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+
+from .views import OrderViewSet
+
+router = routers.DefaultRouter()
+router.register('order', OrderViewSet, 'order')
+
+
+urlpatterns = router.urls
