4fb35ee8a9ef4c49274748f61e4c1c312345c545
Юсу Салихаров <yususaliharov@MacBook-Air.local>
2023-04-20 18:41:31 +0600

    celery

--- apps/order/models.py
+++ apps/order/models.py
@@ -49,4 +49,9 @@ class OrderItem(models.Model):
 def send_order_confirmation_mail(sender: Order, instance: Order, created: bool, **kwargs):
     if created:
         "функция для отправки письма на почту"
-        send_confirmation_mail(instance)
+        send_confirmation_mail.delay(
+            instance.user.username,
+            instance.address,
+            instance.pk,
+            instance.user.email
+        )
--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -13,8 +13,9 @@ class ProductListSerializer(serializers.ListSerializer):
             {
             'title': item.title,
             'slug': item.slug,
-            'user': item.user,
-            'price': item.price
+            'user': item.user.username,
+            'price': item.price,
+            'main_image': item.main_image.url
             } for item in data.all()
         ]
 
@@ -25,12 +26,13 @@ class ProductSerializer(serializers.ModelSerializer):
         write_only=True,
         required=False,
     )
+    user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Product
         fields = '__all__'
         # exclude = 'поле_которое_нужно_пропустить'
-        read_only_fields = ['user', 'slug']
+        read_only_fields = ['slug']
         list_serializer_class = ProductListSerializer
 
     def to_representation(self, instance):
@@ -40,10 +42,11 @@ class ProductSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
-        imgs = validated_data.pop('imgs')
+        imgs = validated_data.pop('imgs', None)
         product = Product.objects.create(**validated_data)
-        images = []
-        for image in imgs:
-            images.append(ProductImage(product=product, image=image))
-        ProductImage.objects.bulk_create(images)
+        if imgs is not None:
+            images = []
+            for image in imgs:
+                images.append(ProductImage(product=product, image=image))
+            ProductImage.objects.bulk_create(images)
         return product
